ecf630243d8577a1bfa90e228ef698eb2a851d46
youngrackchoi <yacksock3@naver.com>
2024-11-27 20:07:39 +0900

    upload page select api url 수정

--- src/main/java/choi/toi/data/ws/controller/v1/UploadController.java
+++ src/main/java/choi/toi/data/ws/controller/v1/UploadController.java
@@ -1,18 +1,18 @@
 package choi.toi.data.ws.controller.v1;
 
 import choi.toi.data.ws.model.support.UploadFileType;
+import choi.toi.data.ws.model.transfer.JobStepTransfers;
 import choi.toi.data.ws.service.v1.UploadService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+@Slf4j
 @RestController
-@RequestMapping("/api/v1/jobs/steps/type/upload")
+@RequestMapping("/api/v1/jobs/steps/type/UPLOAD")
 public class UploadController {
 
     private UploadService uploadService;
@@ -29,6 +29,17 @@ public class UploadController {
                             @RequestParam(value = "workTemplateStepNum") Integer workTemplateStepNum,
                             @RequestParam(value = "inputType") UploadFileType inputType,
                             @RequestParam(value = "files") List<MultipartFile> files) {
+        uploadService.createJobs(userId, workId, workTemplateId, workTemplateStepNum, files, inputType);
+    }
 
+    @GetMapping("/workTemplateId/{workTemplateId}/workTemplateStepNum/{workTemplateStepNum}")
+    public JobStepTransfers getJobStepTransfers(HttpServletRequest request,
+                                                @PathVariable Long workTemplateId,
+                                                @PathVariable Integer workTemplateStepNum,
+                                                @RequestParam(value="userId") Long userId,
+                                                @RequestParam(value="page", required=false, defaultValue="1") Integer page,
+                                                @RequestParam(value="rowsPerPage", required=false, defaultValue="5") Integer rowsPerPage) {
+
+        return uploadService.getJobStepTransfers(workTemplateId, workTemplateStepNum, userId, page, rowsPerPage);
     }
 }
--- src/main/java/choi/toi/data/ws/service/v1/UploadService.java
+++ src/main/java/choi/toi/data/ws/service/v1/UploadService.java
@@ -2,31 +2,36 @@ package choi.toi.data.ws.service.v1;
 
 import choi.toi.data.ws.model.*;
 import choi.toi.data.ws.model.support.JobStepResultStatus;
+import choi.toi.data.ws.model.support.TaskStatusCount;
 import choi.toi.data.ws.model.support.TemplateStepType;
+import choi.toi.data.ws.model.support.UploadFileType;
+import choi.toi.data.ws.model.transfer.JobStepTransfer;
+import choi.toi.data.ws.model.transfer.JobStepTransfers;
 import choi.toi.data.ws.repository.JobRepository;
 import choi.toi.data.ws.repository.WorkTemplateStepRepository;
 import choi.toi.data.ws.service.*;
 import choi.toi.data.ws.service.common.JobStepTaskCommonService;
 import choi.toi.data.ws.util.ExcelUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class UploadService {
 
-
     private JobRepository jobRepository;
-    private JobFileService jobFileService;
     private JobStepService jobStepService;
     private JobStepTaskService jobStepTaskService;
     private JobStepTaskCommonService jobStepTaskCommonService;
     private JobStepTaskWorkerService jobStepTaskWorkerService;
     private JobStepTaskResultService jobStepTaskResultService;
     private JobStepTaskTextService jobStepTaskTextService;
-    private JobStepTaskAudioService jobStepTaskAudioService;
     private TranslationService translationService;
 
     private WorkTemplateStepRepository workTemplateStepRepository;
@@ -35,30 +40,36 @@ public class UploadService {
 
     @Autowired
     public UploadService(JobRepository jobRepository,
-                         JobFileService jobFileService,
                          JobStepService jobStepService,
                          JobStepTaskService jobStepTaskService,
                          JobStepTaskCommonService jobStepTaskCommonService,
                          JobStepTaskWorkerService jobStepTaskWorkerService,
                          JobStepTaskResultService jobStepTaskResultService,
                          JobStepTaskTextService jobStepTaskTextService,
-                         JobStepTaskAudioService jobStepTaskAudioService,
                          TranslationService translationService,
                          WorkTemplateStepRepository workTemplateStepRepository,
                          ExcelUtil excelUtil) {
         this.jobRepository = jobRepository;
-        this.jobFileService = jobFileService;
         this.jobStepService = jobStepService;
         this.jobStepTaskService = jobStepTaskService;
         this.jobStepTaskCommonService = jobStepTaskCommonService;
         this.jobStepTaskWorkerService = jobStepTaskWorkerService;
         this.jobStepTaskResultService = jobStepTaskResultService;
         this.jobStepTaskTextService = jobStepTaskTextService;
-        this.jobStepTaskAudioService = jobStepTaskAudioService;
         this.translationService = translationService;
         this.workTemplateStepRepository = workTemplateStepRepository;
         this.excelUtil = excelUtil;
     }
+
+    @Transactional
+    public void createJobs(Long userId, Long workId, Long workTemplateId, Integer workTemplateStepNum, List<MultipartFile> files, UploadFileType inputType) {
+        log.trace("createJob Start... userId={}, workId={}, workTemplateId={}, workTemplateStepNum={}, files={}, inputType={}", userId, workId, workTemplateId, workTemplateStepNum, files, inputType);
+
+        for (MultipartFile file : files) {
+            createJobByText(userId, workId, workTemplateId, workTemplateStepNum, file);
+        }
+    }
+
 
     public void createJobByText(Long userId, Long workId, Long workTemplateId, Integer workTemplateStepNum, MultipartFile file) {
 //
@@ -67,13 +78,18 @@ public class UploadService {
         jobRepository.insertJob(job);
 
         // file 생성
-        jobFileService.createJobFile(job, file);
+//        jobFileService.createJobFile(job, file);
 
         // JobStep all 생성
         final List<JobStep> createdJobSteps = jobStepService.createJobSteps(job.getId(), workTemplateId);
 
         // task 생성
-        final JobStep jobStep = createdJobSteps.stream().filter(createdJobStep -> createdJobStep.getWorkTemplateStepNum().equals(workTemplateStepNum)).findFirst().get();
+        final JobStep jobStep =
+                createdJobSteps.stream()
+                        .filter(createdJobStep -> createdJobStep.getWorkTemplateStepNum().equals(workTemplateStepNum))
+                        .findFirst()
+                        .get();
+
         final WorkTemplateStep workTemplateStep = workTemplateStepRepository.selectWorkTemplateStep(jobStep.getWorkTemplateId(), jobStep.getWorkTemplateStepNum());
         final JobStepResultStatus status = JobStepResultStatus.Completed;
         final List<JobStepTask> createdJobStepTasks = jobStepTaskService.createJobStepTasks(jobStep.getJobId(), jobStep.getJobStepNum(), workTemplateStep.getInputType(),workTemplateStep.getResultType(), status, sourceTexts.size());
@@ -101,5 +117,9 @@ public class UploadService {
         } else {
             jobStepTaskCommonService.createNextJobStepTasks(jobStepTaskResults);
         }
+    }
+
+    public JobStepTransfers getJobStepTransfers(Long workTemplateId, Integer workTemplateStepNum, Long userId, Integer page, Integer rowsPerPage) {
+        return jobStepService.getJobStepTransfers(workTemplateId, workTemplateStepNum, userId, page, rowsPerPage);
     }
 }
